fc955adc519833e6148938a73dfa193ed1a1ad04
Samuel STPL <samuelstpierrelariviere@gmail.com>
2021-01-25 16:28:20 -0500

    Added data

---
+++ src/Data/skills.js
@@ -0,0 +1,23 @@
+export const ListOfSkillsFE = [
+  { name: "HTML / CSS (Animations & Responsive Design)", score: "80%" },
+  { name: "JavaScript (ES6 & Vanilla)", score: "84%" },
+  { name: "React (Hooks & Context)", score: "75%" },
+  { name: "Redux", score: "60%" },
+  { name: "Styling/ Design", score: "70%" },
+];
+
+export const ListOfSkillsBE = [
+  { name: "Node ", score: "65%" },
+  { name: "Express", score: "63%" },
+  { name: "Python", score: "40%" },
+];
+
+export const ListOfSkillsDB = [
+  { name: "Firebase (Firestore & Auth)", score: "60%" },
+  { name: "MongoDB", score: "50%" },
+];
+
+export const ListOfSkillsTools = [
+  { name: "Styled-Components", score: "80%" },
+  { name: "Git/GitHub", score: "85%" },
+];
