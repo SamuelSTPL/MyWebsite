6ea7bc028dc02801ea73464683affe7cf4b83f15
SamuelSTPL <samuelstpierrelariviere@gmail.com>
2021-02-04 15:07:46 -0500

    Added skills data

--- src/Data/skills.js
+++ src/Data/skills.js
@@ -7,19 +7,22 @@ export const ListOfSkillsFE = [
 ];
 
 export const ListOfSkillsBE = [
-  { name: "Node ", score: "65%" },
-  { name: "Express", score: "63%" },
+  { name: "Node ", score: "70%" },
+  { name: "Express", score: "73%" },
+  { name: "EJS", score: "65%" },
   { name: "Python", score: "40%" },
 ];
 
 export const ListOfSkillsDB = [
   { name: "Firebase (Firestore & Auth)", score: "60%" },
-  { name: "MongoDB", score: "50%" },
+  { name: "MongoDB", score: "60%" },
 ];
 
 export const ListOfSkillsTools = [
   { name: "Styled-Components", score: "80%" },
   { name: "Git/GitHub", score: "85%" },
+  { name: "Netlify", score: "60%" },
+  { name: "Heroku", score: "60%" },
 ];
 
 export const cardItems = [
