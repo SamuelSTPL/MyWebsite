2bfa8262b68f840a7e9c67771710a5ec02eb59d6
SamuelSTPL <samuelstpierrelariviere@gmail.com>
2021-02-06 13:41:56 -0500

    Stored projects in data

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,20 +1,40 @@
+import ezCooking from "../images/EzCooking.png";
+import portfolio from "../images/portfolio.png";
+
 export const listOfProjects = [
-    {
-    name: 'Ez Cooking',
-    size: 'Large',
-    link: 'ez-cooking.netlify.app',
+  {
+    name: "Ez Cooking",
+    image: ezCooking,
+    size: "Large",
+    link: "ez-cooking.netlify.app",
     description: "An easy to use recipe-finder with many filters available",
-    technologies: ['React', 'Redux', 'Express', 'Node', 'FireStore', 'Firebase Auth'],
-    challenges: ["Combine all technologies learned in Concordia' Bootcamp", "Mobile First", "Responsiveness", "Learned how to use authentification"],
-    deployed: ["Netlify", "Heroku"]
-},
-{
-    name: 'Portfolio',
-    size: 'Medium',
-    link: 'https://samuelstpl.netlify.app/',
+    technologies: [
+      "React",
+      "Redux",
+      "Express",
+      "Node",
+      "FireStore",
+      "Firebase Auth",
+    ],
+    challenges: [
+      "Combine all technologies learned in Concordia' Bootcamp",
+      "Mobile First",
+      "Responsiveness",
+      "Learned how to use authentification",
+    ],
+    deployed: ["Netlify", "Heroku"],
+  },
+  {
+    name: "Portfolio",
+    image: portfolio,
+    size: "Medium",
+    link: "https://samuelstpl.netlify.app/",
     description: "My personal web page",
-    technologies: ['React', 'Css Keyframes', 'Styled-components'],
-    challenges: ["Create many custom animations with CSS & JavaScript", "Fully Responsive"],
-    deployed: ["Netlify"]
-    }   
-]
+    technologies: ["React", "Css Keyframes", "Styled-components"],
+    challenges: [
+      "Create many custom animations with CSS & JavaScript",
+      "Fully Responsive",
+    ],
+    deployed: ["Netlify"],
+  },
+];
