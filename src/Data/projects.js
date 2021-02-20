9db7ff6991fee928ac7e312741389635c745b44b
SamuelSTPL <samuelstpierrelariviere@gmail.com>
2021-02-19 21:30:38 -0500

    Data changes

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -3,27 +3,37 @@ import portfolio from "../images/portfolio.png";
 
 export const listOfProjects = [
   {
-    name: "Ez Cooking",
+    name: "Ez Cooking - Front End",
     image: ezCooking,
     size: "Large",
     tooltip: "More than three weeks",
     link: "https://ez-cooking.netlify.app",
     description: "An easy to use recipe-finder with many filters available",
-    technologies: [
-      "React",
-      "Redux",
-      "Express",
-      "Node",
-      "FireStore",
-      "Firebase Auth",
-    ],
+    technologies: ["React", "Redux"],
     challenges: [
-      "Combine all technologies learned in Concordia' Bootcamp",
       "Mobile First",
       "Responsiveness",
-      "Learned how to use authentification",
+      "Makes custom requests to the server depending on wich filters is selected by the user",
+      "The user is able to save favorite recipes",
+      "Using REDUX to store all the recipes Data"
     ],
-    deployed: ["Netlify", "Heroku"],
+    deployed: ["Netlify"]
+  },
+  {
+    name: "Ez Cooking - Back End",
+    image: ezCooking,
+    size: "Large",
+    tooltip: "More than three weeks",
+    link: "https://ez-cooking.netlify.app",
+    description: "An easy to use recipe-finder with many filters available",
+    technologies: ["Express", "Node", "FireStore", "Firebase"],
+    challenges: [
+      "Learned how to use authentification",
+      "Uses of RESTful APIs",
+      "Sorting data received from the Recipe API, before sending everything to the FE",
+      "Using MVC principle"
+    ],
+    deployed: ["Heroku"]
   },
   {
     name: "Portfolio",
@@ -35,8 +45,8 @@ export const listOfProjects = [
     technologies: ["React", "Css Keyframes", "Styled-components"],
     challenges: [
       "Create many custom animations with CSS & JavaScript",
-      "Fully Responsive",
+      "Fully Responsive"
     ],
-    deployed: ["Netlify"],
-  },
+    deployed: ["Netlify"]
+  }
 ];
--- src/Data/skills.js
+++ src/Data/skills.js
@@ -4,20 +4,20 @@ export const ListOfSkillsFE = [
   { name: "React (Hooks & Context)", score: "75%" },
   { name: "Redux", score: "60%" },
   { name: "Typescript", score: "60%" },
-  { name: "Styling/ Design", score: "70%" },
+  { name: "Vue", score: "50%" }
 ];
 
 export const ListOfSkillsBE = [
   { name: "Node ", score: "70%" },
   { name: "Express", score: "73%" },
   { name: "EJS", score: "65%" },
-  { name: "Python", score: "40%" },
+  { name: "Python", score: "40%" }
 ];
 
 export const ListOfSkillsDB = [
   { name: "Firebase (Firestore & Auth)", score: "60%" },
   { name: "MongoDB", score: "60%" },
-  { name: "SQLite", score: "30%" },
+  { name: "SQL", score: "30%" }
 ];
 
 export const ListOfSkillsTools = [
@@ -25,12 +25,12 @@ export const ListOfSkillsTools = [
   { name: "Git/GitHub", score: "85%" },
   { name: "Netlify", score: "60%" },
   { name: "Heroku", score: "60%" },
-  { name: "Linux", score: "65%" },
+  { name: "Linux", score: "65%" }
 ];
 
 export const cardItems = [
   { title: "Front-End", area: ListOfSkillsFE },
   { title: "Back-End", area: ListOfSkillsBE },
   { title: "Databases", area: ListOfSkillsDB },
-  { title: "Tools", area: ListOfSkillsTools },
+  { title: "Tools", area: ListOfSkillsTools }
 ];
